8131fab6d41035a6ac40c90038266126f96edb58
soodvarun78 <soodvarun78@gmail.com>
2017-10-21 11:24:28 +0530

    Initial Commit

---
+++ password.py
@@ -0,0 +1,37 @@
+#A website requires the users to input username and password to register. 
+#Write a program to check the validity of password input by users.
+#Following are the criteria for checking the password:
+#1. At least 1 letter between [a-z]
+#2. At least 1 number between [0-9]
+#1. At least 1 letter between [A-Z]
+#3. At least 1 character from [$#@]
+#4. Minimum length of transaction password: 6
+#5. Maximum length of transaction password: 12
+#Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+#Example
+#If the following passwords are given as input to the program:
+#ABd1234@1,a F1#,2w3E*,2We3345
+#Then, the output of the program should be:
+#ABd1234@1
+
+
+import re
+import string
+s = raw_input();
+l = s.split(',')
+valid = []
+for passwd in l:
+    if not len(passwd) >= 6 and len(passwd) <= 12:
+        continue
+    if re.search('[a-z]',passwd) is None:
+        continue
+    if re.search('[0-9]',passwd) is None:
+        continue
+    if re.search('[A-Z]',passwd) is None:
+        continue
+    if re.search('[$#@]',passwd) is None:
+        continue
+    if re.search('\s',passwd) is not None:
+        continue
+    valid.append(passwd)
+print ",".join(valid)
